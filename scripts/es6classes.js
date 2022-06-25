0e30c00575d1881cd5b8f9523638d125904d1cef
Nataliesmyth <nataliesmyth11@gmail.com>
2022-06-25 17:29:05 -0400

    es6 classes notes

--- scripts/es6classes.js
+++ scripts/es6classes.js
@@ -1,12 +1,39 @@
 // ES6 Class Syntax
 
 class Ship {
-    constructor() {
-        this.id = 1;
-        this.health = 100;
-        this.damage = 10;
+    constructor(id=1, health=100, damage=10, shipColor) {
+        this.id = id;
+        this.health = health;
+        this.damage = damage;
+        this.shipColor = shipColor;
+    }
+    // Instance Method
+    // methods without the static keyword is an instance
+    attack(target) {
+        target.health = target.health - this.damage;
+    }
+
+    // Class Method
+    static getColors() {
+        return ['red', 'blue', 'black'];
     }
 }
 
+// Enemy1 is an Instance of the Ship Class
+// We instantiate the Ship Class
 const enemy1 = new Ship();
 console.log(enemy1)
+const enemy2 = new Ship();
+console.log(enemy2)
+const user = new Ship(10, 50, 5);
+console.log(user)
+
+enemy1.attack(user);
+console.log(user) // health is 40, attack method works
+
+// Ship.attack(); // Ship class CANNOT access instance methods
+
+// Ship.getColors(); // Ship class CAN access static methods
+
+const enemy4 = new Ship(4, 100, 8, Ship.getColors()[1]);
+console.log(enemy4)
