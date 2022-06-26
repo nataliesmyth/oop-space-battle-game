b33dd40e62b4164278416ae20371eeaeb563bbf5
Natalie <nataliesmyth11@gmail.com>
2022-06-26 02:15:58 -0400

    more classes review saving for notes

--- scripts/es6classes.js
+++ scripts/es6classes.js
@@ -37,3 +37,44 @@ console.log(user) // health is 40, attack method works
 
 const enemy4 = new Ship(4, 100, 8, Ship.getColors()[1]);
 console.log(enemy4)
+
+class Person {
+    constructor(firstname, lastname, age, birthdate, birthyear) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+        this.birthdate = birthdate;
+        this.birthyear = birthyear;
+    }
+
+    static sayHi() {
+        return 'hi'
+    }
+}
+console.log(Person.sayHi())
+
+const person0 = new Person('frank', 'degruttalo', 36, 'June 20', 1986)
+
+
+console.log(person0)
+
+class Pet {
+    constructor(name, breed, size, color) {
+        this.name = name;
+        this.breed = breed;
+        this.size = size;
+        this.color = color;
+    }
+
+    static getSizes() {
+        return ['small', 'medium', 'large'];
+    }
+
+    static getFurColor() {
+        return ['black', 'white', 'grey', 'brown', 'red']
+    }
+}
+
+const zoe = new Pet('zoe', 'shih tzu', Pet.getSizes()[1], Pet.getFurColor()[0]);
+
+console.log(zoe)
