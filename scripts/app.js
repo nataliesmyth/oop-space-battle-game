9860e3a296c1d66a6429ead4bfbf60056c6dd9c1
Natalie <nataliesmyth11@gmail.com>
2022-06-26 04:43:31 -0400

    space battle game file

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,41 @@
-class spaceShip {
-    constructor() {
-        hull = 20;
-        firepower = 5;
-        accuracy = .7;
+class SpaceShip {
+    constructor(hull=20, firepower=5, accuracy=.7) {
+        this.hull = hull;
+        this.firepower = firepower;
+        this.accuracy = accuracy;
+    }
+
+    attack(target) {
+        target.health = target.health - this.damage;
     }
 }
+
+console.log(SpaceShip)
+
+const earthCaptain = new SpaceShip();
+
+console.log(earthCaptain)
+
+class Enemy {
+    constructor(hull, firepower, accuracy) {
+        this.hull = hull;
+        this.firepower = firepower;
+        this.accuracy = accuracy;
+    }
+
+    static randomHull(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+    static randomFirepower(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+    static randomAccuracy(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+}
+
+console.log(Enemy.randomHull(3, 6))
+
+const enemy0 = new Enemy(randomHull(3, 6));
+
+console.log(enemy0.randomHull(3, 6))
