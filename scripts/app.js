61567b9553d1c93ce885cf4cc912a58f9b48de70
Nataliesmyth <nataliesmyth11@gmail.com>
2022-06-30 14:44:19 -0400

    fixed random number function to prevent negative integers

--- scripts/app.js
+++ scripts/app.js
@@ -6,13 +6,13 @@ const player = {
     attack: attack,
 }
 
-// functions
+// functions to manipulate properties
 
 function attack(target) {
     target.hull =- this.firepower
 }
 function getRandomNumber (min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
+    return Math.random() * (max - min) + min;
 }
 
 function getRandomDecimal (min, max) {
@@ -33,14 +33,10 @@ class Enemy {
     }
 }
 
-console.log(Enemy)
-// Enemy
-// const alienShip = new Ship();
-
-// Empty Array to populate with aliens
+// Alien Army Array
 const enemyShips = []
 
-// Create your army (populate empty array with desired amount of ships)
+// Loop to populate alien army array
 for (let i = 0; i < 6; i++) {
     enemyShips.push(new Enemy())
 }
@@ -50,17 +46,37 @@ console.log(enemyShips)
 // Player battles each alien one at a time
 // We have to loop through each alien ship until all are defeated, or the player is defeated
 // if hull <= 0, game over
-for (let j = 0; j < enemyShips.length; j++)  {
+// for (let j = 0; j < enemyShips.length; j++)  {
 
-    while (Math.random() > enemyShips[j].accuracy && player.hull <= 0 && enemyShips[j].hull <= 0) {
-        player.attack(enemyShips[j])
-        console.log('you hit the alien ship! Attack again!');
+//     while (Math.random() > enemyShips[j].accuracy && player.hull <= 0 && enemyShips[j].hull <= 0) {
+//         player.attack(enemyShips[j])
+//         console.log('you hit the alien ship! Attack again!');
+//     }
+//     if (Math.random() < enemyShips[j].accuracy) {
+//         console.log('You have been it!');
+//         enemyShips[j].attack(player);
+//         console.log(player)
+//     } 
+// }
+
+// game rounds
+
+function playerMove(enemy) {
+    if (Math.random() < player.accuracy) {
+        player.attack(enemy);
+        return `Direct hit! Enemy ship hull: ${enemy.hull}`
+    } else if (Math.random() > player.accuracy) {
+        enemy.attack(player);
+        return `You missed! Enemy ship hull: ${enemy.hull}`
+    };
+    gameOver();
+};
+
+function gameOver() {
+    for (let i = 0; i < enemyShips.length; i++) {
+        if ((enemyShips[i].hull) <= 0 || (player.hull) <=0) {
     }
-    if (Math.random() < enemyShips[j].accuracy) {
-        console.log('You have been it!');
-        enemyShips[j].attack(player);
-        console.log(player)
-    } 
+        return 'game over'
+    }
 }
-
-
+console.log(playerMove(enemyShips[0]));
