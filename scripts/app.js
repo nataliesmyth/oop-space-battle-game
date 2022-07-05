edbc29e2576e8ed0e78d10cfa5fc6bc9338ed1ff
Nataliesmyth <nataliesmyth11@gmail.com>
2022-07-05 14:28:06 -0400

    alien ship array has functioning ranged properties

--- scripts/app.js
+++ scripts/app.js
@@ -1,53 +1,43 @@
-// player object
-const player = {
-    hull: 20,
-    firepower: 5,
-    accuracy: .7,
-    attack(target) {
-        target.hull = target.hull - this.firepower;
+// Parent Class
+class Ship {
+    constructor(name, hull, firepower, accuracy) {
+        this.name = name;
+        this.hull = hull;
+        this.firepower = firepower;
+        this.accuracy = accuracy;
     }
 }
 
-// functions to manipulate properties
-
-function getRandomNumber (min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-}
-
-console.log(getRandomNumber(3, 6))
-
-function getRandomDecimal (min, max) {
-    if ((min || max) >= 1 || (min || max) < 0) {
-        console.log('please use numbers between 0 and 1')
-    } else {
-        return Math.random() * (max - min) + min;
+// Player Class
+class Player extends Ship {
+    constructor(name, hull, firepower, accuracy) {
+        super(name, hull, firepower, accuracy)
     }
 }
 
-// enemy class
-class Enemy {
-    constructor() {
-        this.hull = getRandomNumber(3, 6);
-        this.firepower = getRandomNumber(2, 4);
-        this.accuracy = getRandomDecimal(.6, .8);
+// Alien Class
+class Alien extends Ship {
+    constructor(name, hull = 3 + Math.floor(Math.random() * 3), firepower = 2 + Math.floor(Math.random() * 2), accuracy = 0.6 + Math.random() * 0.2) {
+        super(name, hull, firepower, accuracy)
     }
-    attack(target) {
-        this.hull = this.hull - this.firepower;
-    };
-};
+}
 
-console.log(Enemy)
+// Player
+const player = new Player('USS Assembly', 20, 5, 0.7);
+
+// Alien Army
+const alienArmy = [];
+
+// Loop to populate army
+for (let i = 0; i < 6; i++) {
+    alienArmy.push(new Alien(`Alien${i + 1}`));
+}
+
+console.log(alienArmy)
 
 
 // Alien Army Array
 const enemyShips = []
-
-// Loop to populate alien army array
-for (let i = 0; i < 6; i++) {
-    enemyShips.push(new Enemy())
-}
-
-console.log(enemyShips[0].hull)
 
 // Player battles each alien one at a time
 // We have to loop through each alien ship until all are defeated, or the player is defeated
@@ -83,4 +73,5 @@ function gameOver() {
     }
         console.log('game over')
 }
+
 console.log(playerMove(enemyShips[0]));
